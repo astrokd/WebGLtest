33e8d750a34daa07b6b8c255ed9cf81ac8b01be5
AstroKD <kdreyerinseattle@gmail.com>
2020-01-24 16:48:55 -0800

    orbitControl and others

--- 3dapp.js
+++ 3dapp.js
@@ -10,7 +10,7 @@ let pTex;
 function setup(){
 
   createCanvas(windowWidth, windowHeight, WEBGL);
-  background(201,32,12);
+  background(51);
 
   pTex = loadImage('img/world.topo.bathy.200412.3x5400x2700.png');
 
@@ -18,12 +18,14 @@ function setup(){
 
 function draw(){
 
-  background(201,32,12);
+  background(51);
 
   epR += 0.1;
-  epZ =map(mouseY, 0, height, 0, -10);
+  epZ =map(0, 0, height, 0, -10);
 
-  pointLight(255,255,255, mouseX,0,400);
+  ambientLight(150);
+  pointLight(255,255,255, 0,0,400);
+  orbitControl();
   renderEPlanet();
 
 }
